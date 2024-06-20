f189a0306b0f3c8606df8ed21a3798bb60e847d9
Ibrahim-Eltoukhy <itokhy@gmail.com>
2024-06-20 03:03:44 +0300

    until lec 6

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const myCoords = [latitude, longitude];
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView(myCoords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(myCoords).addTo(map).bindPopup('Your location.').openPopup();
+    },
+    () => {
+      alert('could not found your location');
+    }
+  );
+}
